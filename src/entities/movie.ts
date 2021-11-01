00e9ec99e53e21b4dff6bb8f3e1c8898a67bc63d
Mario Junior <drapalskimario@gmail.com>
2021-11-01 20:39:13 -0300

    add movie entity

---
+++ src/entities/movie.ts
@@ -0,0 +1,20 @@
+export type Movie = {
+  id: string,
+  name: string,
+  url: string
+  comments: [{
+    authorId: string
+    authorName : string
+    comment: string
+    commentsResponses: {
+      authorId: string
+      authorName: string
+      comment: string
+    }
+  }]
+  ratings: [{
+    authorId: string
+    rating: number
+  }]
+  ratingAverage: number
+}
