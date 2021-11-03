821b3d8ddff6eb9918199e7e2dc0bd82858dd88d
Mario Junior <drapalskimario@gmail.com>
2021-11-03 00:34:17 -0300

    refactor movie schema

--- src/infra/database/schemas/movieSchema.ts
+++ src/infra/database/schemas/movieSchema.ts
@@ -1,31 +1,24 @@
 import { model, Schema } from 'mongoose'
 import { Movie } from '../../../entities/movie'
 
-const commentResponse = new Schema({
-  authorId: { type: String, required: true },
-  authorName: { type: String, required: true },
-  comment: { type: String, required: true }
-})
-
-const commentSchema = new Schema({
-  authorId: { type: String, required: true },
-  authorName: { type: String, required: true },
-  comment: { type: String, required: true },
-  commentsResponses: [commentResponse]
-})
-
-const rating = new Schema({
-  authorId: { type: String, required: true },
-  rating: { type: Number, required: true }
-})
-
 const movieSchema = new Schema<Movie>({
-  id: { type: String, required: true },
   name: { type: String, required: true },
   description: { type: String, required: true },
   url: { type: String, required: true },
-  comments: { type: [commentSchema], required: false },
-  ratings: { tyre: [rating], required: false },
+  comments: [{
+    authorId: { type: String, required: true },
+    authorName: { type: String, required: true },
+    comment: { type: String, required: true },
+    commentsResponses: [{
+      authorId: { type: String, required: true },
+      authorName: { type: String, required: true },
+      comment: { type: String, required: true }
+    }]
+  }],
+  ratings: [{
+    authorId: { type: String, required: true },
+    rating: { type: Number, required: true }
+  }],
   ratingAverage: { type: Number, required: false }
 })
 
